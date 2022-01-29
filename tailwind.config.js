ded7e7ef8f80f2b4e6a034238650bb53aba80f36
Luke Bouch <lukebouch2014@gmail.com>
2022-01-29 16:07:56 -0500

    Setup basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: [
-    'source/**/*.blade.php',
-    'source/**/*.md',
-    'source/**/*.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ["source/**/*.blade.php", "source/**/*.md", "source/**/*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                xs: ".75rem",
+                sm: ".875rem",
+                tiny: ".875rem",
+                base: "1rem",
+                lg: "1.333rem",
+                xl: "1.777rem",
+                "2xl": "2.369rem",
+                "3xl": "3.157rem",
+                "4xl": "4.209rem",
+                "5xl": "5.332rem",
+                "6xl": "6.665rem",
+                "7xl": "7.998rem",
+            },
+            colors: {
+                blue: {
+                    DEFAULT: "0051E0",
+                },
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
