753763710ba71aec825019e400b70ed72fcc5760
Luke Bouch <lukebouch2014@gmail.com>
2022-01-29 16:28:03 -0500

    Add navigation

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.main')
 
 @section('body')
-<section class="flex h-screen p-10 justify-center items-center text-center text-white"
+<section class="flex h-screen p-10 -mt-20 justify-center items-center text-center text-white"
     style="background-image: linear-gradient(111.06deg, rgba(0, 81, 224, 0.75) 0%, rgba(26, 71, 151, 0.75) 100%), url('/assets/images/20220127-WintergreenSunset-02.jpg')">
     <h1>Exceptional Websites<br>
         <div class="text-lg font-normal">designed from the ground up to grow your
