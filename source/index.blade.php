411602fa5315042a22c8fda902d44bbf1309f69d
Luke Bouch <lukebouch2014@gmail.com>
2022-01-29 16:18:11 -0500

    Add hero section

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,11 @@
 @extends('_layouts.main')
 
 @section('body')
-<div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
-</div>
+<section class="flex h-screen p-10 justify-center items-center text-center text-white"
+    style="background-image: linear-gradient(111.06deg, rgba(0, 81, 224, 0.75) 0%, rgba(26, 71, 151, 0.75) 100%), url('/assets/images/20220127-WintergreenSunset-02.jpg')">
+    <h1>Exceptional Websites<br>
+        <div class="text-lg font-normal">designed from the ground up to grow your
+            business.</div>
+    </h1>
+</section>
 @endsection
