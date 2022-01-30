a10d7829dec09be252240fb69127d99d50ffc5eb
Luke Bouch <lukebouch2014@gmail.com>
2022-01-29 21:40:38 -0500

    Add three USP sections

--- source/_components/nav.blade.php
+++ source/_components/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="h-20 w-full p-3 bg-transparent text-white">
-    <div class="container flex justify-between items-center gap-3">
+    <div class="container-xl flex justify-between items-center gap-3">
         <img class="max-w-xs h-full" src="/assets/images/20210727-LukesMediaLogo-White.png" alt="Luke's Media logo">
         <ul class="flex items-center gap-8">
             <li><a href="/">Home</a></li>
