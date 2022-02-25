df2ba036f4436498cd2c88ee92ea3535315f6e16
Luke Bouch <lukebouch2014@gmail.com>
2022-02-25 13:06:18 -0500

    Make logo clickable

--- source/_partials/nav.blade.php
+++ source/_partials/nav.blade.php
@@ -1,6 +1,7 @@
 <nav class="h-20 w-full p-3 text-white {{ $page->getPath() == " /" ? 'bg-transparent' : 'bg-blue' }} ">
     <div class=" container-xl flex flex-wrap justify-between items-center gap-3">
-    <img class="max-w-xs h-full" src="/assets/images/20210727-LukesMediaLogo-White.png" alt="Luke's Media logo">
+    <a href="/"><img class="max-w-xs h-full" src="/assets/images/20210727-LukesMediaLogo-White.png"
+            alt="Luke's Media logo"></a>
     <ul class="flex flex-wrap items-center gap-8">
         <li>
             <a href="/" class="link {{ $page->isActive('/') ? 'link-active' : '' }}">Home</a>
