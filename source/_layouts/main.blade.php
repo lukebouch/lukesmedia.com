e2ea21d7c815609292a24889d8d383cf52830bd3
Luke Bouch <luke@lukebouch.com>
2023-03-12 15:09:10 -0400

    Replace Google Analytics with Fathom analytics

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -11,15 +11,7 @@
     <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
 
     @if($page->production)
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MWLTT0HK7M"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-MWLTT0HK7M');
-    </script>
+    <script src="https://cdn.usefathom.com/script.js" data-site="MASETWQM" defer></script>
     @endif
 </head>
 
